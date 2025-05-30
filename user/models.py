695dec06c32b785b90c62ee65ab3c6970b902c93
HiKKors <nk041496@gmail.com>
2025-05-30 21:00:48 +0400

    Модель пользователя и настройка авторизации

--- user/models.py
+++ user/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class NewClass:
-    None
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    login = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    
+    city = models.CharField(max_length=50)
+    
+    def __str__():
+        return f'{self.name} {self.surname} | {self.login}'
+    
